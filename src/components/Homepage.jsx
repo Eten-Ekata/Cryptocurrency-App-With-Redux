87e675f197375ce1df7b31c749aa2fb6ad8590cf
Eten-Ekata <ekataelishaeten@gmail.com>
2023-06-21 14:51:30 +0100

    middleware added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 const { Title } = Typography;
 
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery();
+  console.log(data)
   return (
     <>
     <Title level={2} className="heading">Global Crypto Stats</Title>
     <Row gutter={[32, 32]}>
-      <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-      <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-      <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
-      <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
-      <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-      <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+      <Col span={12}><Statistic title="Total Cryptocurrencies" value={2} /></Col>
+      <Col span={12}><Statistic title="Total Exchanges" value={millify(9)} /></Col>
+      <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(7)}`} /></Col>
+      <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(7)}`} /></Col>
+      <Col span={12}><Statistic title="Total Cryptocurrencies" value={6} /></Col>
+      <Col span={12}><Statistic title="Total Markets" value={millify(5)} /></Col>
     </Row>
     </>
   )
