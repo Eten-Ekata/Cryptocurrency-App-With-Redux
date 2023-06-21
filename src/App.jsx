9dbf16eed4829a4249d1d13dedda37a501f99196
Eten-Ekata <ekataelishaeten@gmail.com>
2023-06-21 11:21:07 +0100

    footer completed

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,22 @@ return (
           </Routes>
         </div>
       </Layout>
+      <div className="footer">
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
+        </Typography.Title>
+        <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+        </Space>
+      </div>
     </div>
-    <div className='footer'></div>
-
-
-    </div>
-   
-  )
+  </div>
+);
 }
 
 export default App
